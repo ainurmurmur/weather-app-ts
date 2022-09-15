fbabf1fdd6872b4483e33c7cf55afa80b2808437
Ainura <sagandykovaainur@gmail.com>
2022-09-15 14:32:23 +0600

    fix: prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,46 @@
-import React, { FC } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {Layout, Typography} from 'antd';
-import { RootState } from './store';
-import Search from './components/Search';
-import Alert from './components/Alert';
-import Weather from './components/Weather';
-import { setAlert } from './store/actions/alertActions';
-import { setError } from './store/actions/weatherActions';
-import { isDayTime } from './utils/getTime';
-import './index.css';
-
+import React, { FC } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Layout, Typography } from "antd";
+import { RootState } from "./store";
+import Search from "./components/Search";
+import Alert from "./components/Alert";
+import Weather from "./components/Weather";
+import { setAlert } from "./store/actions/alertActions";
+import { setError } from "./store/actions/weatherActions";
+import { isDayTime } from "./utils/getTime";
+import "./index.css";
 
 const App: FC = () => {
   const dispatch = useDispatch();
 
-  const weatherData = useSelector((state:RootState) => state.weather.data)
-  const error = useSelector((state:RootState) => state.weather.error)
-  const loading = useSelector((state:RootState) => state.weather.loading)
-  const alertMessage = useSelector((state:RootState) => state.alert.message)
+  const weatherData = useSelector((state: RootState) => state.weather.data);
+  const error = useSelector((state: RootState) => state.weather.error);
+  const loading = useSelector((state: RootState) => state.weather.loading);
+  const alertMessage = useSelector((state: RootState) => state.alert.message);
   const dayColor = "linear-gradient(rgb(255, 220, 171), rgb(33, 140, 194))";
   const nightColor = "linear-gradient(rgb(90, 50, 255), rgb(4, 15, 117))";
 
   return (
-    <Layout style={{background: isDayTime ? dayColor : nightColor}} className='container'>
-      <Typography.Title style={{color: isDayTime ? "steelblue"  : "wheat"}}>Weather app</Typography.Title>
-    <Search title="Enter city name: " />
-    {loading ? <h2 className="is-size-3 py-2">Loading...</h2> : weatherData && <Weather data={weatherData} />}
+    <Layout
+      style={{ background: isDayTime ? dayColor : nightColor }}
+      className="container"
+    >
+      <Typography.Title style={{ color: isDayTime ? "steelblue" : "wheat" }}>
+        Weather app
+      </Typography.Title>
+      <Search title="Enter city name: " />
+      {loading ? (
+        <h2 className="is-size-3 py-2">Loading...</h2>
+      ) : (
+        weatherData && <Weather data={weatherData} />
+      )}
 
-    {alertMessage && <Alert message={alertMessage} onClose={() => dispatch(setAlert(''))} />}
-    {error && <Alert message={error} onClose={() => dispatch(setError())} />}
-  </Layout>
+      {alertMessage && (
+        <Alert message={alertMessage} onClose={() => dispatch(setAlert(""))} />
+      )}
+      {error && <Alert message={error} onClose={() => dispatch(setError())} />}
+    </Layout>
   );
-}
+};
 
 export default App;
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,60 +1,67 @@
-import React, { FC, FormEvent, useState } from 'react';
-import { useDispatch } from 'react-redux'
-import { setAlert } from '../store/actions/alertActions';
-import { getWeather, setLoading } from '../store/actions/weatherActions';
-import { Button, Form, Row, Input, Typography} from 'antd';
+import React, { FC, FormEvent, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setAlert } from "../store/actions/alertActions";
+import { getWeather, setLoading } from "../store/actions/weatherActions";
+import { Button, Form, Row, Input, Typography } from "antd";
 
 interface SearchProps {
-    title: string
+  title: string;
 }
 
-const Search: FC<SearchProps> =({title}) => {
-    const [ city, setCity ] = useState('');
-    const dispatch = useDispatch();
+const Search: FC<SearchProps> = ({ title }) => {
+  const [city, setCity] = useState("");
+  const dispatch = useDispatch();
 
-    const changeHandler  = (values: any) => {
-        setCity(values?.city)
+  const changeHandler = (values: any) => {
+    setCity(values?.city);
+  };
+
+  const submitHandler = (values: any) => {
+    if (values.city.trim() === "") {
+      return dispatch(setAlert("City is required!"));
     }
 
-    const submitHandler  = (values: any) => {
+    dispatch(setLoading());
+    dispatch(getWeather(values.city) as any);
+    setCity("");
+  };
 
-       if(values.city.trim() === ''){
-            return dispatch(setAlert('City is required!'))
-       }
-
-       dispatch(setLoading())
-       dispatch(getWeather(values.city) as any)
-       setCity('')
-    }
-
-    return (
+  return (
+    <Row>
+      <Row>
         <Row>
-        <Row>
-          <Row>
-            <Typography.Title level={3} style={{color: "white"}}>{title}</Typography.Title>
-            <Form
-                name="basic"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 24 }}
-                initialValues={{ city: '' }}
-                onFinish={submitHandler}
-                autoComplete="off"
-                style={{display: 'inline-flex', marginLeft: 20}}
+          <Typography.Title level={3} style={{ color: "white" }}>
+            {title}
+          </Typography.Title>
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 24 }}
+            initialValues={{ city: "" }}
+            onFinish={submitHandler}
+            autoComplete="off"
+            style={{ display: "inline-flex", marginLeft: 20 }}
+          >
+            <Form.Item
+              name="city"
+              rules={[{ required: true, message: "Please enter city!" }]}
+            >
+              <Input value={city} onChange={changeHandler} />
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button
+                htmlType="submit"
+                type="primary"
+                style={{ background: "white", color: "black" }}
               >
-                  <Form.Item
-                    name="city"
-                    rules={[{ required: true, message: 'Please enter city!' }]}
-                  >
-                    <Input value={city}  onChange={changeHandler}/>
-                  </Form.Item>
-                 <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                  <Button htmlType="submit" type='primary' style={{background: "white", color:'black'}}>Search</Button>
-                 </Form.Item>
-            </Form>
-          </Row>
+                Search
+              </Button>
+            </Form.Item>
+          </Form>
         </Row>
       </Row>
-    )
-}
+    </Row>
+  );
+};
 
-export default Search; 
+export default Search;
